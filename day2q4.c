37306c5daea197330806f0e1a4f28c77f018f3d4
piyushaneja6y <piyushaneja358@gmail.com>
2026-08-21 23:52:22 +0530

    Add files via upload

---
+++ day2q4.c
@@ -0,0 +1,21 @@
+/*Q4 (User Inputs, Operations & Output)
+Write a program to calculate the area and perimeter of a circle given its radius.
+*/
+
+#include <stdio.h>
+#define PI 3.14159
+
+int main() {
+    float radius, area, perimeter;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = PI * radius * radius;
+    perimeter = 2 * PI * radius;
+
+    printf("Area of the circle: %.2f\n", area);
+    printf("Perimeter of the circle: %.2f\n", perimeter);
+
+    return 0;
+}
