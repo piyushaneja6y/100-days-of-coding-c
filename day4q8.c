2c787b949394215caf6bcec2b836ff857e83b87d
piyushaneja6y <piyushaneja358@gmail.com>
2026-08-26 12:35:37 +0530

    Add files via upload

---
+++ day4q8.c
@@ -0,0 +1,12 @@
+/* Q8 (User Inputs, Operations & Output)
+Write a program to find the sum of two numbers */
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum of %d and %d is %d\n", a, b, sum);
+    return 0;
+}
