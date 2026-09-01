041173edb70b9c21f267e343034e140a51db3e81
piyushaneja6y <piyushaneja358@gmail.com>
2026-09-02 00:46:22 +0530

    Add files via upload

---
+++ day15q29.c
@@ -0,0 +1,23 @@
+/*Q29 (Loops without Arrays/Strings)
+Write a program to calculate the factorial of a number*/
+
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    unsigned long long factorial = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        for (i = 1; i <= num; ++i) {
+            factorial *= i;
+        }
+        printf("Factorial of %d = %llu\n", num, factorial);
+    }
+
+    return 0;
+}
