ef40990c4bf52e1e4bd7487b58fbcdffd9676fd9
piyushaneja6y <piyushaneja358@gmail.com>
2026-09-09 12:42:52 +0530

    Add files via upload

---
+++ day21q42.c
@@ -0,0 +1,26 @@
+/*Q42 (Loops without Arrays/Strings)
+Write a program to check if a number is a perfect number*/
+
+
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == num) {
+        printf("%d is a perfect number.\n", num);
+    } else {
+        printf("%d is not a perfect number.\n", num);
+    }
+
+    return 0;
+}
